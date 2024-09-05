fd538051fe04fd06024c93a751c7bf58d91e3f69
Manish <mdashsharma95@gmail.com>
2024-09-05 19:29:44 +0530

    fix: Removed all the unnecessary code

--- backend/app/serializers.py
+++ backend/app/serializers.py
@@ -56,24 +56,3 @@ class UserUpdateSerializer(serializers.Serializer):
     profile_image = serializers.CharField(required=False,allow_blank=True)
 
 
-class ScaleSerializer(serializers.Serializer):
-    workspace_id = serializers.CharField()
-    username = serializers.CharField()
-    scale_name = serializers.CharField()
-    scale_type = serializers.CharField()
-    user_type = serializers.BooleanField()
-    no_of_responses = serializers.IntegerField()
-    channel_instance_list = serializers.ListField()
-    pointers = serializers.IntegerField(required=False)
-    axis_limit = serializers.IntegerField(required=False)
-    redirect_url = serializers.URLField(required=False)
-
-class InstanceDetailsSerializer(serializers.Serializer):
-    instance_name = serializers.CharField()
-    instance_display_name = serializers.CharField()
-
-
-class ChannelInstanceSerializer(serializers.Serializer):
-    channel_name = serializers.CharField()
-    channel_display_name = serializers.CharField()
-    instances_details = InstanceDetailsSerializer(many=True)
--- backend/app/urls.py
+++ backend/app/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import UserManagement, ScaleManagement,healthCheck, ScaleCreateAPI
+from .views import *
 
 urlpatterns = [
     path("", healthCheck.as_view()),
     path("voc/user-management/",UserManagement.as_view()),
-    path("voc/scale-management/",ScaleManagement.as_view()),
-    path("scale/scale-services/",ScaleCreateAPI.as_view())
+    path("voc/scale-management/",ScaleManagement.as_view())
 
 ]

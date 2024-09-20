5086bc773feda6e8c00366ba629d86a656941433
Manish <mdashsharma95@gmail.com>
2024-09-20 19:43:34 +0530

    feat: Merging all the branch and deployment

--- backend/core/views.py
+++ backend/core/views.py
@@ -31,6 +31,34 @@ class RedirectURL(APIView):
         except Exception as e:
             print("Exception:", e)
             return redirect("https://dowellresearch.sg/")
+@method_decorator(csrf_exempt, name='dispatch')
+class RedirectURLForLLx(APIView):
+    def get(self, request):
+        workspace_id = request.GET.get('workspace_id')
+        username = request.GET.get('username')
+        scale_id = request.GET.get('scale_id')
+        scale_type = request.GET.get('scale_type')
+        channel_name = request.GET.get('channel')
+        instance_name = request.GET.get('instance_name')
+        channel_display_name = request.GET.get('channel_display_name')
+        instance_display_name = request.GET.get('instance_display_name')
+
+        if not all([workspace_id, username, scale_id, scale_type, channel_name, instance_name, channel_display_name, instance_display_name]):
+            return redirect("https://dowellresearch.sg/")
+
+        try:
+            link = (
+                f"https://www.scales.uxlivinglab.online/llx/scale/?"
+                f"workspace_id={workspace_id}&username={username}&"
+                f"scale_id={scale_id}&scale_type={scale_type}&"
+                f"channel={channel_name}&instance_name={instance_name}&"
+                f"channel_display_name={channel_display_name}&instance_display_name={instance_display_name}"
+            )
+
+            return redirect(link)
+        except Exception as e:
+            print("Exception:", e)
+            return redirect("https://dowellresearch.sg/")
 
 @method_decorator(csrf_exempt, name='dispatch')
 class ReportRedirectURL(APIView):
@@ -54,6 +82,27 @@ class ReportRedirectURL(APIView):
             return redirect("https://dowellresearch.sg/")
 
 @method_decorator(csrf_exempt, name='dispatch')
+class ReportRedirectURLForLLx(APIView):
+    def get(self, request):
+        workspace_id = request.GET.get('workspace_id')
+        username = request.GET.get('username')
+        scale_id = request.GET.get('scale_id')
+
+        if not all([workspace_id, username, scale_id]):
+            return redirect("https://dowellresearch.sg/")
+
+        try:
+            report_link = (
+                f"https://www.scales.uxlivinglab.online/llx/report/?"
+                f"workspace_id={workspace_id}&username={username}&scale_id={scale_id}"
+            )
+
+            return redirect(report_link)
+        except Exception as e:
+            print("Exception:", e)
+            return redirect("https://dowellresearch.sg/")
+
+@method_decorator(csrf_exempt, name='dispatch')
 class LoginsRedirectURL(APIView):
     def get(self, request):
         workspace_name = request.GET.get('workspace_name')
@@ -65,6 +114,28 @@ class LoginsRedirectURL(APIView):
         try:
             login_link = (
                 f"https://www.scales.uxlivinglab.online/voc/?workspace_name={workspace_name}"
+            )
+
+            if portfolio:
+                login_link += f"&portfolio={portfolio}"
+
+            return redirect(login_link)
+        except Exception as e:
+            print("Exception:", e)
+            return redirect("https://dowellresearch.sg/")
+
+@method_decorator(csrf_exempt, name='dispatch')
+class LoginsRedirectURLForLLx(APIView):
+    def get(self, request):
+        workspace_name = request.GET.get('workspace_name')
+        portfolio = request.GET.get('portfolio')
+
+        if not workspace_name:
+            return redirect("https://dowellresearch.sg/")
+        
+        try:
+            login_link = (
+                f"https://www.scales.uxlivinglab.online/llx/?workspace_name={workspace_name}"
             )
 
             if portfolio:
